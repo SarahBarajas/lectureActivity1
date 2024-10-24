d53dcb371596de2acf4308ffb215d4977ebc8010
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-10-23 19:20:00 -0700

    added comments

--- script.js
+++ script.js
@@ -4,25 +4,32 @@ this
 is
 a
 multi-line
-cpment
+comment
 */
 //console.log("hello world'');
 
-
+// 1. 8 * null = ???
 console.log(8 * null); // 0
 
+// 2. “5” - 1 = ???
 console.log("5" - 1); // result: 4
 
+// 3. “5” + 1 = ???
 console.log("5" + 1); // result: "51"
 
+// 4. “five” * 2 = ???
 console.log("five" * 2); // result: NaN
 
+// 5. false == 0 = ???
 console.log(false == 0); // result: true
 
+// 6. 5 > 2 && 6 > 3 = ???
 console.log(5 > 2 && 6 > 3); // result: true
 
+// 7.false == false ? “equal” : “not equal” = ???
 console.log(false == false ? "equal" : "not equal"); // result: "equal"
 
+// 8. true || false = ???
 console.log(true || false); // result: true
 
 
