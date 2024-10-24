49e88d8dd8d0bac62c535ff796d77d58c4c3d1b9
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-10-23 19:15:42 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//this is a scomment line 
+/*
+this 
+is
+a
+multi-line
+cpment
+*/
+//console.log("hello world'');
+
+
+console.log(8 * null); // 0
+
+console.log("5" - 1); // result: 4
+
+console.log("5" + 1); // result: "51"
+
+console.log("five" * 2); // result: NaN
+
+console.log(false == 0); // result: true
+
+console.log(5 > 2 && 6 > 3); // result: true
+
+console.log(false == false ? "equal" : "not equal"); // result: "equal"
+
+console.log(true || false); // result: true
+
+
